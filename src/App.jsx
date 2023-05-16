c9dc3fa13fb432d50be544808d07235e0035c39e
Jannek <janneks@tutanota.com>
2023-05-16 23:09:49 +0200

    Clean up App component.

--- src/App.jsx
+++ src/App.jsx
@@ -40,13 +40,7 @@ function App() {
       <Paragraphs />
       <div className="card">
         <AppButton value={"Click Me"} type={"button"} />
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
