d66adafb3403746bc6a4a2aaa32c49aae76ea486
Jannek <janneks@tutanota.com>
2023-05-21 14:02:26 +0200

    Changed behaviour of menu items on hover.

--- homework-lesson2/src/AppSidebar.jsx
+++ homework-lesson2/src/AppSidebar.jsx
@@ -3,7 +3,7 @@ import MenuItem from "./MenuItem";
 function AppSidebar({ menuItems }) {
   return (
     <aside className="w-1/4 h-full border-slate-400 border-r p-4">
-      <ul className="flex flex-col gap-2 font-semibold">
+      <ul className="flex flex-col gap-3 font-semibold">
         {menuItems.map((item) => (
           <MenuItem name={item.name} url={item.url} />
         ))}
--- homework-lesson2/src/MenuItem.jsx
+++ homework-lesson2/src/MenuItem.jsx
@@ -1,6 +1,6 @@
 function MenuItem({ name, url }) {
   return (
-    <li key={name}>
+    <li className="hover:scale-105 hover:ml-1 duration-300" key={name}>
       <a className="text-slate-700 hover:text-slate-900" href={url}>
         {name}
       </a>
