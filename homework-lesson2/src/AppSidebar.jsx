d317cbc9f52a0d1adbf167e36aecaf1d984fc673
Jannek <janneks@tutanota.com>
2023-05-21 21:31:21 +0200

    Fixed issue with key in rendered list of menu items.

--- homework-lesson2/src/AppSidebar.jsx
+++ homework-lesson2/src/AppSidebar.jsx
@@ -5,7 +5,7 @@ function AppSidebar({ menuItems }) {
     <aside className="w-1/4 h-full border-slate-400 border-r p-4">
       <ul className="flex flex-col gap-3 font-semibold">
         {menuItems.map((item) => (
-          <MenuItem name={item.name} url={item.url} />
+          <MenuItem name={item.name} url={item.url} key={item.name} />
         ))}
       </ul>
     </aside>
--- homework-lesson2/src/MenuItem.jsx
+++ homework-lesson2/src/MenuItem.jsx
@@ -1,6 +1,6 @@
 function MenuItem({ name, url }) {
   return (
-    <li className="hover:scale-105 hover:ml-1 duration-300" key={name}>
+    <li className="hover:scale-105 hover:ml-1 duration-300">
       <a
         className="text-slate-700 hover:text-indigo-800 hover:drop-shadow-lg"
         href={url}
