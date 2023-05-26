dbc0d395cc134a20a1da02d6e79db98b95b2aaaf
Jannek <janneks@tutanota.com>
2023-05-26 23:21:05 +0200

    Styled message component and added expand and collapse on button click.

--- homework-lesson2/src/Message.jsx
+++ homework-lesson2/src/Message.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
+import AppButton from "./AppButton";
+
 function Message({ author, text, status }) {
-  let msgStatus = status;
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div>
-      <h3>from: {author}</h3>
-      <p>{text}</p>
+    <div className="p-4 rounded-lg bg-indigo-50">
+      <h3 className="text-md text-indigo-700 font-bold">{author}:</h3>
+      <p className={!isOpen && "line-clamp-2"}>{text}</p>
+      <div className="flex flex-row space-x-2">
+        <div className="flex-1"></div>
+        {/* {isOpen && (
+          <AppButton onClick={() => } text="mark as read" />
+        )} */}
+        <AppButton
+          onClick={() => setIsOpen(!isOpen)}
+          text={isOpen ? "Close" : "Read message"}
+        />
+      </div>
     </div>
   );
 }
