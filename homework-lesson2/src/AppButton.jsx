38b5d0744491cc099afca6281c87dafc24e44a26
Jannek <janneks@tutanota.com>
2023-05-26 23:17:48 +0200

    Added prop to change width of AppButton.

--- homework-lesson2/src/AppButton.jsx
+++ homework-lesson2/src/AppButton.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 
-function AppButton({ text, onClick }) {
+function AppButton({ text, width, onClick }) {
   return (
     <button
       onClick={onClick}
-      className="w-44 p-2 my-1 bg-indigo-300 text-slate-800 text-xs rounded-md shadow-lg shadow-indigo-300 hover:bg-indigo-700 hover:text-indigo-50 active:shadow-sm active:bg-indigo-500 duration-150"
+      className={`w-${width} p-2 my-1 bg-indigo-300 text-slate-800 text-xs rounded-md shadow-md shadow-indigo-100 hover:bg-indigo-700 hover:text-indigo-50 active:shadow-sm active:bg-indigo-500 duration-150`}
     >
       {text}
     </button>
