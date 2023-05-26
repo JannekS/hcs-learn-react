69622fdc6e18b72cdb8ab6a260317eee0a055346
Jannek <janneks@tutanota.com>
2023-05-26 23:19:21 +0200

    Changed styling of inbox component.

--- homework-lesson2/src/Inbox.jsx
+++ homework-lesson2/src/Inbox.jsx
@@ -2,19 +2,19 @@ import Message from "./Message";
 
 function Inbox({ unreadMessages }) {
   return (
-    <>
-      <h2>Inbox</h2>
+    <div className="border-slate-400 border-t py-4">
+      <h2 className="mb-4 text-lg font-bold">Inbox</h2>
       <ul className="flex flex-col gap-3 w-full h-96 overflow-auto">
         {unreadMessages.map((msg) => (
           <Message
             author={msg.author}
-            text={msg.content}
+            text={msg.text}
             status={msg.status}
             key={msg.id}
           />
         ))}
       </ul>
-    </>
+    </div>
   );
 }
 
