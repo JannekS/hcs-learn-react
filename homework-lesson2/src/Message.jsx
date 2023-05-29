168b12de856e40b6c235ca80f7f75a8f641dadbc
Jannek <janneks@tutanota.com>
2023-05-29 13:39:35 +0200

    Fixed issue with conditional rendering in Meesage component.

--- homework-lesson2/src/Message.jsx
+++ homework-lesson2/src/Message.jsx
@@ -7,12 +7,15 @@ function Message({ author, text, status }) {
   return (
     <div className="p-4 rounded-lg bg-indigo-50">
       <h3 className="text-md text-indigo-700 font-bold">{author}:</h3>
-      <p className={!isOpen && "line-clamp-2"}>{text}</p>
+      <p className={isOpen ? "" : "line-clamp-2"}>{text}</p>
       <div className="flex flex-row space-x-2">
         <div className="flex-1"></div>
-        {/* {isOpen && (
-          <AppButton onClick={() => } text="mark as read" />
-        )} */}
+        {isOpen && (
+          <AppButton
+            onClick={() => console.log("Message has been read.")}
+            text="mark as read"
+          />
+        )}
         <AppButton
           onClick={() => setIsOpen(!isOpen)}
           text={isOpen ? "Close" : "Read message"}
