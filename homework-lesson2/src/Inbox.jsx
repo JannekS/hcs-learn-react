42602b294cfeef8e84e02a5622a528d00f3791f1
Jannek <janneks@tutanota.com>
2023-05-29 15:18:20 +0200

    Implemented functionality to mark message as read.

--- homework-lesson2/src/Inbox.jsx
+++ homework-lesson2/src/Inbox.jsx
@@ -1,16 +1,25 @@
 import Message from "./Message";
 
-function Inbox({ unreadMessages }) {
+function Inbox({ inboxMessages, updateMessage }) {
   return (
     <div className="border-slate-400 border-t py-4">
       <h2 className="mb-4 text-lg font-bold">Inbox</h2>
       <ul className="flex flex-col gap-3 w-full h-96 overflow-auto">
-        {unreadMessages.map((msg) => (
+        {inboxMessages.map((msg) => (
           <Message
             author={msg.author}
             text={msg.text}
             status={msg.status}
             key={msg.id}
+            id={msg.id}
+            markAsRead={() =>
+              updateMessage({
+                id: msg.id,
+                author: msg.author,
+                text: msg.text,
+                status: "read",
+              })
+            }
           />
         ))}
       </ul>
--- homework-lesson2/src/Message.jsx
+++ homework-lesson2/src/Message.jsx
@@ -1,20 +1,19 @@
 import { useState } from "react";
 import AppButton from "./AppButton";
 
-function Message({ author, text, status }) {
+function Message({ author, text, status, id, markAsRead }) {
   const [isOpen, setIsOpen] = useState(false);
+  let msgReadClass =
+    status === "read" ? "" : "border-solid border-2 border-indigo-700";
 
   return (
-    <div className="p-4 rounded-lg bg-indigo-50">
+    <div className={`p-4 rounded-lg bg-indigo-50 ${msgReadClass}`}>
       <h3 className="text-md text-indigo-700 font-bold">{author}:</h3>
       <p className={isOpen ? "" : "line-clamp-2"}>{text}</p>
       <div className="flex flex-row space-x-2">
         <div className="flex-1"></div>
-        {isOpen && (
-          <AppButton
-            onClick={() => console.log("Message has been read.")}
-            text="mark as read"
-          />
+        {isOpen && status === "unread" && (
+          <AppButton onClick={markAsRead} text="mark as read" />
         )}
         <AppButton
           onClick={() => setIsOpen(!isOpen)}
