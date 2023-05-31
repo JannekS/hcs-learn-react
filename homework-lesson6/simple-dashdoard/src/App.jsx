b4514f5f5bbe6d42597f2a57825d4afe9b3e66b2
Jannek <janneks@tutanota.com>
2023-05-31 19:26:51 +0200

    Fetch todo data from jsonplaceholder api.

--- homework-lesson6/simple-dashdoard/src/App.jsx
+++ homework-lesson6/simple-dashdoard/src/App.jsx
@@ -1,10 +1,25 @@
-import { useState } from "react";
-import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  async function fetchTodoData() {
+    const res = await fetch(
+      "https://jsonplaceholder.typicode.com/todos?userId=1"
+    );
+    const todoData = await res.json();
+    setTodos(todoData);
+  }
+
+  useEffect(() => {
+    fetchTodoData();
+    console.log(todos);
+  }, []);
+
   return (
     <>
       <h1>My Simple Dashboard</h1>
+      <h2>{todos.length}</h2>
     </>
   );
 }
