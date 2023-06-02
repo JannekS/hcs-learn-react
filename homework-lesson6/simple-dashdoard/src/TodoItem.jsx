018cc5a831444d1538fcfcd3869bc2c0f7a5c896
Jannek <janneks@tutanota.com>
2023-06-02 09:17:50 +0200

    Added controlled checkbox to mark and unmark items as completed.

--- homework-lesson6/simple-dashdoard/src/TodoItem.jsx
+++ homework-lesson6/simple-dashdoard/src/TodoItem.jsx
@@ -1,6 +1,18 @@
+import { useState } from "react";
+
 function TodoItem({ todo }) {
-  let doneClass = todo.completed ? "line-through text-gray-300" : "";
-  return <li className={`${doneClass} w-80`}>{todo.title}</li>;
+  const [completed, setCompleted] = useState(todo.completed);
+  let doneClass = completed ? "line-through text-gray-300" : "";
+  return (
+    <li className={`flex flex-row space-x-4  items-center w-80 ${doneClass}`}>
+      <input
+        type="checkbox"
+        checked={completed}
+        onChange={() => setCompleted(!completed)}
+      />
+      <p>{todo.title}</p>
+    </li>
+  );
 }
 
 export default TodoItem;
